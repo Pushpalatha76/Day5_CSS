f6f49d17c4849a89ff56a441fe0c62050cb95399
Boya Pushpalatha <60311622+Pushpalatha76@users.noreply.github.com>
2021-10-14 18:44:05 +0530

    Add files via upload

---
+++ Demo_Exception_handling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Exception_handling
+{
+    class Class1
+    {
+    }
+
+    /// <summary>
+    ///  User Define Exception Handling using Base Exception Class 
+    /// </summary>
+    [Serializable]
+    public class StudentNotFoundException : Exception
+    {
+        public string StudentName { get; set; }
+        public StudentNotFoundException() { }
+        public StudentNotFoundException(string message) : base(message) {}
+
+        //Console.WriteLine(" Calling base class message defined in Exception class");
+
+        public StudentNotFoundException(string Message, Exception InnerException) : base(Message, InnerException) { }
+
+        public StudentNotFoundException(string message, string studentName)
+       : this(message)
+        {
+            StudentName = studentName;
+        }
+    }
+}
